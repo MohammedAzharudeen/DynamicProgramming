bca5eebbd770d4f2d1441306a0a7ea8399dc030d
Mohammed Azharudeen A <42969244+MohammedAzharudeen@users.noreply.github.com>
2021-05-26 12:16:46 +0530

    Update WildcardPatternMatching.java

--- WildcardPatternMatching.java
+++ WildcardPatternMatching.java
@@ -33,6 +33,7 @@ public class WildcardPatternMatching {
                 }
             }else{
                 pattern[writeIndex++] = pattern[i];
+		isFirst = true;
             }
         }
         
